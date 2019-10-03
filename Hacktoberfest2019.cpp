1bb73d66a59ce65c41f886a2a60563f49c52759d
35601 <twdavis@kithfurniture.com>
2019-10-03 10:07:57 -0500

    35601

---
+++ Hacktoberfest2019.cpp
@@ -0,0 +1,13 @@
+#include<iostream>  
+  
+using namespace std; 
+  
+// main function - 
+// where the execution of program begins 
+int main() 
+{ 
+     
+    cout<<"HacktoberFest2019"; 
+      
+    return 0; 
+} 
